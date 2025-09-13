45b8b2beedd894abef24cdb070f06ad8a8b173bb
Hardik140430 <hardikharsora1404@gmail.com>
2025-09-13 20:52:00 +0530

    new commit

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -207,7 +207,7 @@ function renderPreview() {
   submitBtn.classList.add("btn-submit");
   submitBtn.onclick = async () => {
     try {
-      const response = await fetch("https://perpetual-blessing-production.up.railway.app/user", {
+      const response = await fetch("https://pleasing-purpose-production.up.railway.app/user", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(userData)
